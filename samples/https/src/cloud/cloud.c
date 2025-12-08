2fcaea7b79f35aeafd840fecdf3c81c60a9a62c9
Marjon <marjonz@gmail.com>
2025-12-09 07:30:44 +1300

    Bug fix from circuitdojo's main branch

--- samples/https/src/cloud/cloud.c
+++ samples/https/src/cloud/cloud.c
@@ -96,16 +96,16 @@ int tls_setup(int fd)
 
 static int socket_setup()
 {
-    int fd;
+    int fd = -1;
     int err = 0;
-    struct addrinfo *res;
+    struct addrinfo *res = NULL;
 
     /* Hints */
     struct addrinfo hints =
-        {
-            .ai_family = AF_INET,
-            .ai_socktype = SOCK_STREAM,
-        };
+    {
+        .ai_family = AF_INET,
+        .ai_socktype = SOCK_STREAM,
+    };
 
     LOG_INF("Looking up %s", CONFIG_CLOUD_HOSTNAME);
     err = getaddrinfo(CONFIG_CLOUD_HOSTNAME, NULL, &hints, &res);
@@ -184,13 +184,21 @@ static int socket_setup()
         LOG_ERR("Unable to connect. Err: %i - %s", err, strerror(errno));
     }
     else
+    {
         LOG_INF("Connected!");
+    }
 
 clean_up:
-    freeaddrinfo(res);
-    if (err < 0 && fd > -1)
+    if (res != NULL)
     {
-        (void)close(fd);
+        LOG_INF("Free addr info");
+        freeaddrinfo(res);
+        res = NULL;
+    }
+
+    if (err < 0 && fd >= 0)
+    {
+        close(fd);
         fd = -1;
     }
 
@@ -266,10 +274,10 @@ int cloud_publish(struct device_data *data)
     fd = socket_setup();
     if (fd < 0)
     {
-        k_free(msg);
+        cJSON_free(msg);
 
-        LOG_ERR("Unable to setup socket. Err: %i", ret);
-        return ret;
+        LOG_ERR("Unable to setup socket. Err: %i", fd);
+        return fd;
     }
 
     LOG_INF("Socket setup complete");
@@ -300,9 +308,11 @@ int cloud_publish(struct device_data *data)
     if (ret < 0)
         LOG_ERR("Unable to send data to cloud. Err: %i", ret);
 
-    /* Close connection */
-    (void)close(fd);
-
+    if (fd)
+    {
+        /* Close connection */
+        (void)close(fd);
+    }
 
     /* Free data */
     cJSON_free(msg);
