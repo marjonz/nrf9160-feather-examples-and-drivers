bb8a9de14c7589e5c310461efd813ae433d8fa5b
Marjon <marjonz@gmail.com>
2025-12-18 12:48:06 +1300

    WIP: stashing GNSS work

--- fleetpin/src/main.c
+++ fleetpin/src/main.c
@@ -62,6 +62,30 @@ static void lte_handler(const struct lte_lc_evt *evt)
     }
 }
 
+/* Initialization of AUX pin */
+#if defined(CONFIG_BOARD_CIRCUITDOJO_FEATHER_NRF9151)
+#define AUXANTCFG_ENABLE "AT\%XANTCFG=1"
+
+NRF_MODEM_LIB_ON_INIT(aux_init_hook, on_modem_lib_init, NULL);
+
+static void on_modem_lib_init(int ret, void *ctx)
+{
+    ARG_UNUSED(ctx);
+
+    if (ret != 0)
+    {
+        return;
+    }
+
+    printk("*** Setting configuration: %s ***\n", AUXANTCFG_ENABLE);
+    int err = nrf_modem_at_printf("%s", AUXANTCFG_ENABLE);
+    if (err)
+    {
+        LOG_ERR("Failed to set configuration (err: %d)", err);
+    }
+}
+#endif
+
 int main(void)
 {
     int err;
