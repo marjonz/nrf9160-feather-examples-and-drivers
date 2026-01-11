2f891348e1d0ed427dabdb27f7ec2a37dec25004
Marjon <marjonz@gmail.com>
2026-01-12 11:26:11 +1300

    Add the epaper interface for use. Add supporting UI modules for drawing on the epaper.

---
+++ fleetpin/src/epaper/epaper.c
@@ -0,0 +1,29 @@
+#include "epaper_driver.h"
+#include "GUI_Paint.h"
+
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(epaper);
+
+//static const size_t Imagesize = ((EPD_4in26_WIDTH % 8 == 0)? (EPD_4in26_WIDTH / 8 ): (EPD_4in26_WIDTH / 8 + 1)) * EPD_4in26_HEIGHT;
+#define MAX_IMAGE_SIZE (EPD_4in26_WIDTH * EPD_4in26_HEIGHT)
+//Create a new image cache
+static uint8_t DisplayImage[MAX_IMAGE_SIZE] = {0};
+
+int epaper_init(void)
+{
+    //EPD_4in26_Init();
+    EPD_4in26_Init_4GRAY();
+    EPD_4in26_Clear();
+    k_msleep(500);
+
+    return 0;
+}
+
+void epaper_display_test(void)
+{
+    Paint_NewImage(DisplayImage, EPD_4in26_WIDTH, EPD_4in26_HEIGHT, 0, BLACK);
+    EPD_4in26_Display_Fast(DisplayImage);
+    k_msleep(2000);
+
+}
---
+++ fleetpin/src/epaper/epaper.h
@@ -0,0 +1,20 @@
+/***********************************************************************
+ * @file    epaper.h
+ * @author  Flexware Ltd
+ * @date    Jan 2026
+ * @brief   Header file for ePaper display functions
+ ***********************************************************************/
+
+/**
+ * @brief Initialize the ePaper display
+ *
+  * @return int 0 if successful, negative errno otherwise
+ *
+ */
+int epaper_init(void);
+
+/**
+ * @brief API used to display test image
+ *
+ */
+void epaper_display_test(void);
